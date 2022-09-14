552bd561014042328177dfb4135bba3698577d76
Cyrus <Cyrusgitau08@gmail.com>
2022-09-14 06:45:23 +0300

    Initial commit

---
+++ app/src/main/java/com/example/eclectics/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.eclectics
+
+import android.content.Intent
+import android.hardware.biometrics.BiometricManager
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
+import java.util.concurrent.Executor
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var executor: Executor
+    private lateinit var biometricPrompt: BiometricPrompt
+    private lateinit var promptInfo: BiometricPrompt.PromptInfo
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // init biometrics
+        executor = ContextCompat.getMainExecutor(this)
+        biometricPrompt = BiometricPrompt(this, executor,
+            object : BiometricPrompt.AuthenticationCallback() {
+                override fun onAuthenticationError(errorCode: Int,
+                                                   errString: CharSequence) {
+                    super.onAuthenticationError(errorCode, errString)
+
+                    Toast.makeText(applicationContext,
+                        "Authentication error: $errString", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+                override fun onAuthenticationSucceeded(
+                    result: BiometricPrompt.AuthenticationResult) {
+                    super.onAuthenticationSucceeded(result)
+                    startActivity(Intent(this@MainActivity,HomeActivity::class.java))
+                    Toast.makeText(applicationContext,
+                        "Authentication succeeded!", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+                override fun onAuthenticationFailed() {
+                    super.onAuthenticationFailed()
+                    Toast.makeText(applicationContext, "Authentication failed",
+                        Toast.LENGTH_SHORT)
+                        .show()
+                }
+            })
+
+        promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Biometric login for my app")
+            .setSubtitle("Log in using your biometric credential")
+            .setNegativeButtonText("Use account password")
+            .build()
+
+
+        ///click listener
+        val authBtn: Button = findViewById(R.id.authBtn)
+        authBtn.setOnClickListener{
+            biometricPrompt.authenticate(promptInfo)
+
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/eclectics/news_feed/News_Feed.kt
@@ -0,0 +1,4 @@
+package com.example.eclectics.news_feed
+
+class News_Feed {
+}
